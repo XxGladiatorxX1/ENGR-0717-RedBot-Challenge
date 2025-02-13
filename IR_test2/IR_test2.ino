d102a2838268f9dc71c15083416b6ac1ad662ba0
gberg25 <geb126@pitt.edu>
2025-02-13 18:16:26 -0500

    created IR sensor file

---
+++ IR_test2/IR_test2.ino
@@ -0,0 +1,68 @@
+
+//  IMPORTS
+#include <RedBot.h>
+
+//  INSTANCE VARIABLES
+/* Sensor, encoder, and motor object initializations. Note that sensor constructors 
+ * are used to instantiate no particular sensor
+ */
+RedBotSensor leftIRSensor = RedBotSensor(A3);
+RedBotSensor centerIRSensor = RedBotSensor(A6);
+RedBotSensor rightIRSensor = RedBotSensor(A7);
+RedBotEncoder encoder = RedBotEncoder(A2, 10);
+RedBotMotors motors;
+
+//  Variables to store motor speeds
+int leftMotorSpeed;
+int rightMotorSpeed;
+
+//  CONSTANTS
+//  The IR sensor values, measured 1/8" from the ground
+#define BLACK_MIN 900
+#define BLACK_MAX 1100
+#define RED_MIN 50
+#define RED_MAX 200
+#define BROWN_MIN 400
+#define BROWN_MAX 600
+
+
+//  Nominal speeds for the motors (max is 255)
+#define SLOW_SPEED 10
+#define FAST_SPEED 100
+#define REV_SLOW -10
+#define REV_FAST -100
+
+void setup()
+{
+    // Initialize baudrate
+    Serial.begin(9600);
+
+    //  Motor encoder calibration
+    encoder.clearEnc(BOTH);
+}
+
+void loop()
+{
+    //  Provides IR readings through serial monitor
+    Serial.print(leftIRSensor.read());
+    Serial.print("\t");
+    Serial.print(centerIRSensor.read());
+    Serial.print("\t");
+    Serial.print(rightIRSensor.read());
+    delay(1000);
+    Serial.println();
+
+    if(leftIRSensor.read==0 && rightIRSensor.read==0 && centerIRSensor.read==0) {
+        drive(FAST_SPEED);
+    }
+    else if (leftIRSensor.read>900 && rightIRSensor<900){
+        rightMotor(SLOW_SPEED);
+        leftMotor(REV_SLOW);
+    }
+    else if (leftIRSensor.read<900 && rightIRSensor>900) {
+        leftMotor(SLOW_SPEED);
+        rightMotor(REV_SLOW);
+    }
+}
+
+
